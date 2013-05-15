a445c98d26b9c7487944275614d7cbac9bedc5f8
Sinh Pham <phamansinh@gmail.com>
2013-05-15 12:30:49 -0600

    Parser.

--- SteamSpecialsWp/ViewModel/MainViewModel.cs
+++ SteamSpecialsWp/ViewModel/MainViewModel.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Xml.Serialization;
 using GalaSoft.MvvmLight;
 using GalaSoft.MvvmLight.Messaging;
+using HtmlAgilityPack;
 
 namespace SteamSpecialsWp.ViewModel
 {
@@ -31,7 +32,8 @@ namespace SteamSpecialsWp.ViewModel
 
     public class SteamSpecialItemViewModel
     {
-        public SteamSpecialItemViewModel(SteamSpecialItem item) {
+        public SteamSpecialItemViewModel(SteamSpecialItem item)
+        {
             Name = item.Name;
             ImgUrl = item.ImgUrl;
             Link = item.Link;
@@ -48,13 +50,17 @@ namespace SteamSpecialsWp.ViewModel
             double op;
             double np;
             if (double.TryParse(OldPrice.Substring(1), out op) &&
-                double.TryParse(NewPrice.Substring(1), out np)) {
+                double.TryParse(NewPrice.Substring(1), out np))
+            {
                 var per = (np - op) / op * 100;
                 per = Math.Round(per);
                 SalePercentage = per.ToString() + "%";
-                if (per <= -75) {
+                if (per <= -75)
+                {
                     SaleColor = new SolidColorBrush(Colors.Red);
-                } else if (per <= -50) {
+                }
+                else if (per <= -50)
+                {
                     SaleColor = new SolidColorBrush(Colors.Green);
                 }
             }
@@ -76,14 +82,17 @@ namespace SteamSpecialsWp.ViewModel
     public class MainViewModel : ViewModelBase, IVMState<MainViewModel.StateData>
     {
         readonly ObservableCollection<SteamSpecialItemViewModel> _ss = new ObservableCollection<SteamSpecialItemViewModel>();
-        public ObservableCollection<SteamSpecialItemViewModel> SS {
-            get {
+        public ObservableCollection<SteamSpecialItemViewModel> SS
+        {
+            get
+            {
                 return _ss;
             }
         }
         List<SteamSpecialItem> _itemList = new List<SteamSpecialItem>();
 
-        public MainViewModel() {
+        public MainViewModel()
+        {
             DataLoaded = false;
             PageNum = 1;
             IsRefreshing = false;
@@ -91,23 +100,31 @@ namespace SteamSpecialsWp.ViewModel
 
         Random _randomId = new Random();
         bool _isRefreshing;
-        bool IsRefreshing {
-            get {
+        bool IsRefreshing
+        {
+            get
+            {
                 return _isRefreshing;
             }
-            set {
+            set
+            {
                 _isRefreshing = value;
-                if (_isRefreshing) {
+                if (_isRefreshing)
+                {
                     ProgressBarMess = "Loading data...";
                     ShowProgressBar = true;
-                } else {
+                }
+                else
+                {
                     ProgressBarMess = "";
                     ShowProgressBar = false;
                 }
             }
         }
-        public async void Refresh() {
-            if (IsRefreshing) {
+        public async void Refresh()
+        {
+            if (IsRefreshing)
+            {
                 return;
             }
             IsRefreshing = true;
@@ -116,16 +133,19 @@ namespace SteamSpecialsWp.ViewModel
             RaisePropertyChanged("InfoText");
 
             var wc = new SharpGIS.GZipWebClient();
-            var url = "http://steamspecialsweb.apphb.com/SteamSpecials?id=" + _randomId.Next();
+            var url = "http://store.steampowered.com/search/?sort_by=Name&sort_order=ASC&specials=1";
 
             string res = "";
             var downloadTask = wc.DownloadStringTaskAsync(url);
 
             await Task.WhenAny(downloadTask, Task.Delay(20000));
 
-            if (downloadTask.Status == TaskStatus.RanToCompletion) {
+            if (downloadTask.Status == TaskStatus.RanToCompletion)
+            {
                 res = downloadTask.Result;
-            } else {
+            }
+            else
+            {
                 // Error downloading data.
                 IsRefreshing = false;
                 ProgressBarMess = "Error downloading data. Try refresh again.";
@@ -133,16 +153,14 @@ namespace SteamSpecialsWp.ViewModel
             }
 
             var bw = new BackgroundWorker();
-            bw.DoWork += (bwSender, bwArg) => {
-                var byteArray = Encoding.Unicode.GetBytes(res);
-                using (var stream = new MemoryStream(byteArray)) {
-                    var xs = new XmlSerializer(typeof(List<SteamSpecialItem>));
-                    try {
-                        _itemList = (List<SteamSpecialItem>)xs.Deserialize(stream);
-                    } catch (Exception) {
-                    }
-                }
-                Deployment.Current.Dispatcher.BeginInvoke(() => {
+            bw.DoWork += (bwSender, bwArg) =>
+            {
+                _itemList.Clear();
+                var htmlDoc = new HtmlDocument();
+                htmlDoc.LoadHtml(res);
+                SSParser.ProcessSearchDoc(htmlDoc, _itemList);
+                Deployment.Current.Dispatcher.BeginInvoke(() =>
+                {
                     PageNum = 1;
                     IsRefreshing = false;
                 });
@@ -150,44 +168,56 @@ namespace SteamSpecialsWp.ViewModel
             bw.RunWorkerAsync();
         }
 
-        public bool DataLoaded {
+        public bool DataLoaded
+        {
             get;
             set;
         }
-        public double ListVerticalOffset {
+        public double ListVerticalOffset
+        {
             get;
             set;
         }
 
         bool _showProgressBar;
-        public bool ShowProgressBar {
-            get {
+        public bool ShowProgressBar
+        {
+            get
+            {
                 return _showProgressBar;
             }
-            set {
+            set
+            {
                 _showProgressBar = value;
                 RaisePropertyChanged("ShowProgressBar");
             }
         }
 
         string _progressbarMess;
-        public string ProgressBarMess {
-            get {
+        public string ProgressBarMess
+        {
+            get
+            {
                 return _progressbarMess;
             }
-            set {
+            set
+            {
                 _progressbarMess = value;
                 RaisePropertyChanged("ProgressBarMess");
             }
         }
 
         int _pageNum;
-        public int PageNum {
-            get {
+        public int PageNum
+        {
+            get
+            {
                 return _pageNum;
             }
-            set {
-                if (value < 1 || (value - 1) * ItemPerPage >= _itemList.Count) {
+            set
+            {
+                if (value < 1 || (value - 1) * ItemPerPage >= _itemList.Count)
+                {
                     return;
                 }
                 _pageNum = value;
@@ -197,36 +227,47 @@ namespace SteamSpecialsWp.ViewModel
 
                 var newIdx = minIdx;
                 var oldIdx = 0;
-                while (newIdx < maxIdx && newIdx < _itemList.Count) {
+                while (newIdx < maxIdx && newIdx < _itemList.Count)
+                {
                     var ssivm = new SteamSpecialItemViewModel(_itemList[newIdx]);
-                    if (oldIdx < SS.Count) {
+                    if (oldIdx < SS.Count)
+                    {
                         SS[oldIdx] = ssivm;
-                    } else {
+                    }
+                    else
+                    {
                         SS.Add(ssivm);
                     }
                     ++newIdx;
                     ++oldIdx;
                 }
-                while (oldIdx != SS.Count) {
+                while (oldIdx != SS.Count)
+                {
                     SS.RemoveAt(oldIdx);
                 }
 
                 RaisePropertyChanged("InfoText");
             }
         }
-        public string InfoText {
-            get {
+        public string InfoText
+        {
+            get
+            {
                 var minIdx = (PageNum - 1) * ItemPerPage + 1;
                 var maxIdx = minIdx + SS.Count - 1;
-                if (maxIdx < minIdx) {
+                if (maxIdx < minIdx)
+                {
                     FooterInfoText = "";
                     return "Showing 0 - 0 of 0";
                 }
 
                 var more = _itemList.Count - maxIdx;
-                if (more > 0) {
+                if (more > 0)
+                {
                     FooterInfoText = more.ToString() + " more";
-                } else {
+                }
+                else
+                {
                     FooterInfoText = "";
                 }
 
@@ -234,18 +275,22 @@ namespace SteamSpecialsWp.ViewModel
             }
         }
         string _footerInfoText;
-        public string FooterInfoText {
-            get {
+        public string FooterInfoText
+        {
+            get
+            {
                 return _footerInfoText;
             }
-            set {
+            set
+            {
                 _footerInfoText = value;
                 RaisePropertyChanged("FooterInfoText");
             }
         }
         static int ItemPerPage = 25;
         // Tombstone support.
-        public Boolean NeedSaveState {
+        public Boolean NeedSaveState
+        {
             get;
             set;
         }
@@ -257,9 +302,12 @@ namespace SteamSpecialsWp.ViewModel
             public int PageNum;
         }
 
-        public StateData CurrentState {
-            get {
-                if (!NeedSaveState) {
+        public StateData CurrentState
+        {
+            get
+            {
+                if (!NeedSaveState)
+                {
                     return null;
                 }
                 var state = new StateData();
@@ -269,7 +317,8 @@ namespace SteamSpecialsWp.ViewModel
                 return state;
             }
         }
-        public void LoadFromState(StateData state) {
+        public void LoadFromState(StateData state)
+        {
             NeedSaveState = true;
             _itemList = state.ItemList;
             PageNum = state.PageNum;
---
+++ SteamSpecialsWp/ViewModel/SSParser.cs
@@ -0,0 +1,180 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SteamSpecialsWp.ViewModel
+{
+    public static class SSParser
+    {
+        //public static List<HtmlDocument> ProcessExtraPage(HtmlDocument htmlDoc)
+        //{
+        //    var ret = new List<HtmlDocument>();
+        //    if (htmlDoc.ParseErrors != null && htmlDoc.ParseErrors.Count() > 0)
+        //    {
+
+        //    }
+        //    else
+        //    {
+        //        if (htmlDoc.DocumentNode != null)
+        //        {
+        //            var searchPages = htmlDoc.DocumentNode.SelectSingleNode("//div[@class='search_pagination_right']");
+        //            if (searchPages != null)
+        //            {
+        //                var pages = searchPages.SelectNodes("a");
+        //                if (pages != null)
+        //                {
+        //                    var pageUrls = new List<string>();
+        //                    foreach (var page in pages)
+        //                    {
+        //                        pageUrls.Add(page.GetAttributeValue("href", ""));
+        //                    }
+        //                    pageUrls.RemoveAt(pageUrls.Count - 1);
+        //                    var lastUrl = pageUrls[pageUrls.Count - 1];
+        //                    var num = int.Parse(lastUrl.Substring(lastUrl.IndexOf("page=") + 5));
+        //                    var url = "http://store.steampowered.com/search/?sort_by=&sort_order=ASC&specials=1&page=";
+        //                    pageUrls.Clear();
+        //                    if (num > 10)
+        //                    {
+        //                        // Only get 20 first pages.
+        //                        num = 10;
+        //                    }
+        //                    for (var i = 2; i <= num; ++i)
+        //                    {
+        //                        pageUrls.Add(url + i.ToString());
+        //                    }
+                            
+        //                    var extraDocs = new List<HtmlDocument>();
+        //                    Parallel.ForEach(pageUrls, pu =>
+        //                    {
+        //                        var pageStr = "";
+        //                        var wc = new WebClient();
+        //                        try
+        //                        {
+        //                            pageStr = wc.DownloadString(pu);
+        //                        }
+        //                        catch (System.Net.WebException)
+        //                        {
+        //                        }
+        //                        var currHtmlDoc = new HtmlDocument();
+        //                        currHtmlDoc.LoadHtml(pageStr);
+        //                        lock (ret)
+        //                        {
+        //                            ret.Add(currHtmlDoc);
+        //                        }
+        //                    });
+                            
+        //                }
+        //            }
+        //        }
+        //    }
+        //    return ret;
+        //}
+
+        public static void ProcessSearchDoc(HtmlDocument htmlDoc, List<SteamSpecialItem> retList)
+        {
+            if (htmlDoc.ParseErrors != null && htmlDoc.ParseErrors.Count() > 0)
+            {
+                return;
+            }
+            if (htmlDoc.DocumentNode != null)
+            {
+                var searchResults = htmlDoc.DocumentNode.SelectSingleNode("//div[@id='search_results']");
+
+                if (searchResults != null)
+                {
+                    var even = searchResults.SelectNodes("//a[@class='search_result_row even']");
+                    var odd = searchResults.SelectNodes("//a[@class='search_result_row odd']");
+                    
+                    if (even != null && odd != null)
+                    {
+                        var max = Math.Max(even.Count, odd.Count);
+                        for (var i = 0; i < max; ++i)
+                        {
+                            if (i < even.Count)
+                            {
+                                retList.Add(ParseNode(even[i]));
+                            }
+                            if (i < odd.Count)
+                            {
+                                retList.Add(ParseNode(odd[i]));
+                            }
+                        }
+                    }
+                }
+            }
+            
+        }
+
+        static SteamSpecialItem ParseNode(HtmlNode currNode)
+        {
+            var ret = new SteamSpecialItem();
+            try
+            {
+                var name = currNode.SelectSingleNode("div[@class='col search_name ellipsis']/h4");
+                if (name != null)
+                {
+                    ret.Name = HtmlEntity.DeEntitize(name.InnerText);
+                }
+
+                var imgLink = currNode.SelectSingleNode("div[@class='col search_capsule']/img");
+                if (imgLink != null)
+                {
+                    ret.ImgUrl = imgLink.GetAttributeValue("src", "nolink");
+                }
+
+                var link = currNode.GetAttributeValue("href", "nolink");
+                ret.Link = link;
+
+                var oldPrice = currNode.SelectSingleNode("div[@class='col search_price']/span/strike");
+                if (oldPrice != null)
+                {
+
+                    ret.OldPrice = HtmlEntity.DeEntitize(oldPrice.InnerText);
+                }
+
+                var newPrice = currNode.SelectSingleNode("div[@class='col search_price']/text()");
+                if (newPrice != null)
+                {
+                    ret.NewPrice = HtmlEntity.DeEntitize(newPrice.InnerText);
+                }
+
+                var metaScore = currNode.SelectSingleNode("div[@class='col search_metascore']");
+                if (metaScore != null)
+                {
+                    ret.MetaScore = HtmlEntity.DeEntitize(metaScore.InnerText);
+                }
+
+                var typeImg = currNode.SelectSingleNode("div[@class='col search_type']/img");
+                if (typeImg != null)
+                {
+                    ret.TypeImg = typeImg.GetAttributeValue("src", "nolink");
+                }
+
+                var platformImg = currNode.SelectSingleNode("div[@class='col search_name ellipsis']/p/img");
+                if (platformImg != null)
+                {
+                    ret.PlatformImg = platformImg.GetAttributeValue("src", "nolink");
+                }
+
+                var cat_release = platformImg.NextSibling;
+                if (cat_release != null)
+                {
+                    ret.Cat_Release = HtmlEntity.DeEntitize(cat_release.InnerText);
+                }
+            }
+            catch (Exception e)
+            {
+            }
+            if (string.IsNullOrEmpty(ret.OldPrice) || string.IsNullOrEmpty(ret.NewPrice))
+            {
+                ret.OldPrice = "$-1";
+                ret.NewPrice = "$-1";
+            }
+            return ret;
+        }
+    }
+}
